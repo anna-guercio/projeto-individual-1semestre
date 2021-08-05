27ac93d40b5e230f6f168ef718bc42d4f47a9779
anna-guercio <anna.guercio@bandtec.com.br>
2021-08-04 22:15:41 -0300

    Projeto Individual 1 Semestre

---
+++ projetoindividual-1semestre/projeto-site/config/config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  development: {
+    dialect: "sqlite",
+    storage: "./db.development.sqlite"
+  },
+  test: {
+    dialect: "sqlite",
+    storage: ":memory:"
+  },
+  production: {
+    username: 'adm',
+    password: 'Melancia13!',
+    database: 'bdAnna',
+    host: 'annaguercio.database.windows.net',
+    dialect: 'mssql',
+    xuse_env_variable: 'DATABASE_URL',
+    dialectOptions: {
+      options: {
+        encrypt: true
+      }
+    },
+    pool: { 
+      max: 5,
+      min: 1,
+      acquire: 5000,
+      idle: 30000,
+      connectTimeout: 5000
+    }
+  }
+};
+ 
+
+ 
